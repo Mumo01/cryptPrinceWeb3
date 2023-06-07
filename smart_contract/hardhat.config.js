05e7f0505e0e0ad784aee00442301c19b6b4fbb1
Mumo01 <55277139+Mumo01@users.noreply.github.com>
2023-06-07 16:55:39 +0300

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks:  {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/bFx7PP15f9iGFa5KtID0RO2WN1bRyhhV',
-      accounts: [ '51a1431f6936b5b3f9a65b1e4fa4d454c59f30f8dd7599eb12baa7298bf88b0d' ]
+      accounts: [ 'privatekey' ]
     },
   },
 };
